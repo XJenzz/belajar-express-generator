647118ca468a34564967cc8bf323abec03d114b6
Axenior <matiusstephen813@gmail.com>
2024-10-22 10:28:33 +0700

    menambahkan konfigurasi env

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
-const {Key, DB} = require("../../api_key");
+
+const KEY = process.env.KEY;
+const DB = process.env.DB;
 
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      `mongodb://${Key}@cluster0-shard-00-00.qeqfs.mongodb.net:27017,cluster0-shard-00-01.qeqfs.mongodb.net:27017,cluster0-shard-00-02.qeqfs.mongodb.net:27017/${DB}?ssl=true&replicaSet=atlas-wwygfo-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`
+      `mongodb://${KEY}@cluster0-shard-00-00.qeqfs.mongodb.net:27017,cluster0-shard-00-01.qeqfs.mongodb.net:27017,cluster0-shard-00-02.qeqfs.mongodb.net:27017/${DB}?ssl=true&replicaSet=atlas-wwygfo-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`
     );
     console.log("MonggoDB connect");
   } catch (e) {
