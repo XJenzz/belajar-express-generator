689cef653f286c5b82199d8474025ea5768ac621
Axenior <matiusstephen813@gmail.com>
2024-10-22 11:18:34 +0700

    membuat api untuk prodi

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -7,7 +7,8 @@ const DB = process.env.DB;
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      `mongodb://${KEY}@cluster0-shard-00-00.qeqfs.mongodb.net:27017,cluster0-shard-00-01.qeqfs.mongodb.net:27017,cluster0-shard-00-02.qeqfs.mongodb.net:27017/${DB}?ssl=true&replicaSet=atlas-wwygfo-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`
+      // `mongodb://${KEY}@cluster0-shard-00-00.qeqfs.mongodb.net:27017,cluster0-shard-00-01.qeqfs.mongodb.net:27017,cluster0-shard-00-02.qeqfs.mongodb.net:27017/${DB}?ssl=true&replicaSet=atlas-wwygfo-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`
+      `mongodb+srv://${KEY}@cluster0.qeqfs.mongodb.net/${DB}?retryWrites=true&w=majority&appName=Cluster0`
     );
     console.log("MonggoDB connect");
   } catch (e) {
