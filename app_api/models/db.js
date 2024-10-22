a649db87c71b2a30dcd9f02b99a02838932edf94
Axenior <matiusstephen813@gmail.com>
2024-10-22 10:00:00 +0700

    menambahkan git ignore api key

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
+const {Key, DB} = require("../../api_key");
 
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb://Axenior:PAW2@cluster0-shard-00-00.qeqfs.mongodb.net:27017,cluster0-shard-00-01.qeqfs.mongodb.net:27017,cluster0-shard-00-02.qeqfs.mongodb.net:27017/mdpsi5a?ssl=true&replicaSet=atlas-wwygfo-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
+      `mongodb://${Key}@cluster0-shard-00-00.qeqfs.mongodb.net:27017,cluster0-shard-00-01.qeqfs.mongodb.net:27017,cluster0-shard-00-02.qeqfs.mongodb.net:27017/${DB}?ssl=true&replicaSet=atlas-wwygfo-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`
     );
     console.log("MonggoDB connect");
   } catch (e) {
